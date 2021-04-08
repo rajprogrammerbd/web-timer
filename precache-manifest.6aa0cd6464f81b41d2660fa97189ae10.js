2957b74b6e3308b3f898739904b60a9fedf677d6
Raj Dutta <rd2249619@gmail.com>
2021-04-08 11:59:48 +0600

    Updates

---
+++ precache-manifest.6aa0cd6464f81b41d2660fa97189ae10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2d08ac75c0c3ab6b7d564777ba3a0c",
+    "url": "/web-timer/index.html"
+  },
+  {
+    "revision": "df8d9ed8b8a955b81449",
+    "url": "/web-timer/static/css/main.3abbcd46.chunk.css"
+  },
+  {
+    "revision": "0abf76000c3501389fec",
+    "url": "/web-timer/static/js/2.facd3739.chunk.js"
+  },
+  {
+    "revision": "b399bb2b2ea08630cb75b076e919735c",
+    "url": "/web-timer/static/js/2.facd3739.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df8d9ed8b8a955b81449",
+    "url": "/web-timer/static/js/main.6de97919.chunk.js"
+  },
+  {
+    "revision": "84248cd1320377754f00",
+    "url": "/web-timer/static/js/runtime-main.69f7a435.js"
+  },
+  {
+    "revision": "3e74f665b324ee1429e5df65b38b67f1",
+    "url": "/web-timer/static/media/alarm_clock.3e74f665.mp3"
+  }
+]);
